10ec33172a45c4b996671da41d0692cf454e5dff
Ronald <rrnewball@gmail.com>
2023-11-20 17:37:43 -0500

    add variables 1 and 2 and result

--- programa1.py
+++ programa1.py
@@ -1,1 +1,8 @@
 #Primer ejemplo de python y git
+variable1=0
+variable2=0
+
+variable1=int(input("Digita 1° valor: "))
+variable2=int(input("Digita 2° valor: "))
+suma=variable1+variable2
+print(suma)
